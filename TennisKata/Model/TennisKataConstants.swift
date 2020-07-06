9f6e7a6001f762bbb8c8f554304b50e61b3f443f
Dev-Challenge-3 <>
2020-07-06 21:42:08 +0100

    Refactored Game Tests

---
+++ TennisKata/Model/TennisKataConstants.swift
@@ -0,0 +1,8 @@
+struct TennisKataConstants {
+    
+    static let fifteenLove = "Player 1 fifteen - Player 2 love"
+    static let thirtyLove = "Player 1 thirty - Player 2 love"
+    static let fortyLove = "Player 1 forty - Player 2 love"
+    
+}
+
--- TennisKataTests/Model/GameTests.swift
+++ TennisKataTests/Model/GameTests.swift
@@ -3,52 +3,60 @@ import XCTest
 
 class GameTests: XCTestCase {
 
-    var playerOne = Player(playerName: "Player 1")
-    var playerTwo = Player(playerName: "Player 2")
+    var playerOne: Player!
+    var playerTwo: Player!
     var game: Game!
 
     override func setUpWithError() throws {
+        super.setUp()
+        playerOne = Player(playerName: "Player 1")
+        playerTwo = Player(playerName: "Player 2")
+        game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
     }
 
     override func tearDownWithError() throws {
+        super.tearDown()
+        playerOne = nil
+        playerTwo = nil
+        game = nil
+    }
+    
+    private func playerServeWins(player: SelectedPlayer, times: Int){
+        for _ in 1...times {
+            game.playerPlays(selectedPlayer: player)
+        }
     }
     
     //MARK: - Test case for the score 15 - 0
 
     func testGame_GameScoreAfterPlayerOneScoresFifteen_ShouldReturnTrue() {
         //Arrange
-        game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
-        game.playerPlays(selectedPlayer: .playerOne)
+        playerServeWins(player: .playerOne, times: 1)
         //Act
         let gameScore = game.score()
         //Assert
-        XCTAssertTrue(gameScore == "Player 1 fifteen - Player 2 love", "Should return game score as Player 1 fifteen - Player 2 love")
+        XCTAssertTrue(gameScore == TennisKataConstants.fifteenLove, "Should return game score as \(TennisKataConstants.fifteenLove)")
     }
     
     //MARK: - Test case for the score 30 - 0
     
     func testGame_GameScoreAfterPlayerOneScoresThirty_ShouldReturnTrue() {
         //Arrange
-        game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
-        game.playerPlays(selectedPlayer: .playerOne)
-        game.playerPlays(selectedPlayer: .playerOne)
+        playerServeWins(player: .playerOne, times: 2)
         //Act
         let gameScore = game.score()
         //Arrange
-        XCTAssertTrue(gameScore == "Player 1 thirty - Player 2 love", "Should return game score as Player 1 thirty - Player 2 love")
+        XCTAssertTrue(gameScore == TennisKataConstants.thirtyLove, "Should return game score as \(TennisKataConstants.thirtyLove)")
     }
     
     //MARK: - Test cases for the score 40 - 0
 
     func testGame_GameScoreAfterPlayerOneScoresForty_ShouldReturnScore() {
         //Arrange
-        game = Game(firstPlayer: playerOne, secondPlayer: playerTwo)
-        game.playerPlays(selectedPlayer: .playerOne)
-        game.playerPlays(selectedPlayer: .playerOne)
-        game.playerPlays(selectedPlayer: .playerOne)
+        playerServeWins(player: .playerOne, times: 3)
         //Act
         let gameScore = game.score()
         //Arrange
-        XCTAssertTrue(gameScore == "Player 1 forty - Player 2 love", "Should return game score as Player 1 forty - Player 2 love")
+        XCTAssertTrue(gameScore == TennisKataConstants.fortyLove, "Should return game score as \(TennisKataConstants.fortyLove)")
     }
 }
